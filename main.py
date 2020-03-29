5781de0ce20bbeb809e0ccd7a0b3b201569f999a
fartbagxp <fartbagxp@gmail.com>
2020-03-29 04:27:27 -0400

    Simple scraping for GCP and Zoom (#2)

--- main.py
+++ main.py
@@ -2,56 +2,115 @@ import pytricia
 from pprint import pprint
 from src.parser_aws import AWSCIDRParser
 from src.parser_azure import AzureCIDRParser
+from src.parser_gcp import GoogleCIDRParser
+from src.parser_zoom import ZoomCIDRParser
+
+
+def add_aws_cidr(pytrie, result):
+  for prefix in result['prefixes']:
+    pyt[prefix['ip_prefix']] = {
+        'region': prefix['region'],
+        'service': prefix['service']
+    }
+
+  for prefix in result['ipv6_prefixes']:
+    pyt[prefix['ipv6_prefix']] = {
+        'region': prefix['region'],
+        'service': prefix['service']
+    }
+
+
+def add_azure_cidr(pytrie, result):
+  for value in result['values']:
+    for prefix in value['properties']['addressPrefixes']:
+      pyt[prefix] = {
+          'region': value['properties']['region'],
+          'platform': value['properties']['platform'],
+          'systemService': value['properties']['systemService'],
+          'cloud': result['cloud']
+      }
+
+
+def add_gcp_cidr(pytrie, result):
+  for value in result['data']:
+    pyt[value] = {
+        'source': result['source'],
+        'last_updated': result['last_updated']
+    }
+
+
+def add_zoom_cidr(pytrie, result):
+  for values in result['data']:
+    for value in values['Destination']:
+      try:
+        pyt[value] = {
+            'source': result['source'],
+            'last_updated': result['last_updated'],
+            'connectionType': values['ConnectionType'],
+            'protocol': values['Protocol'],
+            'ports': values['Ports'],
+            'sourceName': values['SourceName']
+        }
+      except:
+        '''
+        Best effort scraping the data for IP CIDR ranges
+        '''
+        pass
+
 
 aws_parser = AWSCIDRParser()
 result = aws_parser.get_range()
-
 pyt = pytricia.PyTricia()
-for prefix in result['prefixes']:
-  pyt[prefix['ip_prefix']] = {
-      'region': prefix['region'],
-      'service': prefix['service']
-  }
-
-for prefix in result['ipv6_prefixes']:
-  pyt[prefix['ipv6_prefix']] = {
-      'region': prefix['region'],
-      'service': prefix['service']
-  }
+add_aws_cidr(pyt, result)
 
 '''
 Testing - AWS
 {'region': 'us-east-1', 'service': 'EC2'}
 {'region': 'me-south-1', 'service': 'AMAZON'}
 '''
-# print(pyt.get('52.95.245.0'))
-# print(pyt.get('2a05:d07e:e000::'))
+print(pyt.get('52.95.245.0'))
+print(pyt.get('2a05:d07e:e000::'))
 
 azure_parser = AzureCIDRParser()
-result = azure_parser.get_range()
-for value in result['values']:
-  for prefix in value['properties']['addressPrefixes']:
-    pyt[prefix] = {
-        'region': value['properties']['region'],
-        'platform': value['properties']['platform'],
-        'systemService': value['properties']['systemService'],
-        'cloud': result['cloud']
-    }
+result = azure_parser.get_public_range()
+add_azure_cidr(pyt, result)
+
+result = azure_parser.get_china_range()
+add_azure_cidr(pyt, result)
+
+result = azure_parser.get_germany_range()
+add_azure_cidr(pyt, result)
 
 result = azure_parser.get_gov_range()
-for value in result['values']:
-  for prefix in value['properties']['addressPrefixes']:
-    pyt[prefix] = {
-        'region': value['properties']['region'],
-        'platform': value['properties']['platform'],
-        'systemService': value['properties']['systemService'],
-        'cloud': result['cloud']
-    }
+add_azure_cidr(pyt, result)
 
 '''
 Testing - Azure
 {'region': '', 'platform': 'Azure', 'systemService': '', 'cloud': 'Public'}
-{'region': '', 'platform': 'Azure', 'systemService': 'AzureAppConfiguration', 'cloud': 'AzureGovernment'}
+{'region': '', 'platform': 'Azure',
+    'systemService': 'AzureAppConfiguration', 'cloud': 'AzureGovernment'}
 '''
-# print(pyt.get('213.199.183.0'))
-# print(pyt.get('52.127.61.112'))
+print(pyt.get('213.199.183.0'))
+print(pyt.get('52.127.61.112'))
+
+gcp_parser = GoogleCIDRParser()
+result = gcp_parser.get_range()
+add_gcp_cidr(pyt, result)
+
+'''
+Testing - Google
+
+'''
+print(pyt.get('35.199.128.0'))
+print(pyt.get('35.200.0.0'))
+
+zoom_parser = ZoomCIDRParser()
+result = zoom_parser.get_range()
+add_zoom_cidr(pyt, result)
+
+'''
+Testing - Zoom
+
+'''
+print(pyt.get('3.80.20.128'))
+print(pyt.get('103.122.166.0'))
--- src/parser_azure.py
+++ src/parser_azure.py
@@ -1,7 +1,9 @@
 '''
 
 Azure Public Cloud: https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519
-Azure GovCloud: https://www.microsoft.com/en-us/download/details.aspx?id=57063
+Azure China Cloud: https://www.microsoft.com/en-us/download/details.aspx?id=57062
+Azure US Government Cloud: https://www.microsoft.com/en-us/download/details.aspx?id=57063
+Azure Germany Cloud: https://www.microsoft.com/en-us/download/details.aspx?id=57064
 '''
 
 
@@ -9,22 +11,44 @@ import requests
 
 
 class AzureCIDRParser():
-  def get_range(self):
+  def get_public_range(self):
     try:
-      URL = 'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200316.json'
+      URL = 'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20200323.json'
       r = requests.get(url=URL)
       data = r.json()
       return data
     except requests.exceptions.RequestException as e:
-      print(f'Failure to scrape AWS IP range endpoint, error was {e}')
+      print(f'Failure to scrape Azure Public IP range endpoint, error was {e}')
+      return None
+
+  def get_china_range(self):
+    try:
+      URL = 'https://download.microsoft.com/download/9/D/0/9D03B7E2-4B80-4BF3-9B91-DA8C7D3EE9F9/ServiceTags_China_20200323.json'
+      r = requests.get(url=URL)
+      data = r.json()
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Azure China IP range endpoint, error was {e}')
+      return None
+
+  def get_germany_range(self):
+    try:
+      URL = 'https://download.microsoft.com/download/0/7/6/076274AB-4B0B-4246-A422-4BAF1E03F974/ServiceTags_AzureGermany_20200323.json'
+      r = requests.get(url=URL)
+      data = r.json()
+      return data
+    except requests.exceptions.RequestException as e:
+      print(
+          f'Failure to scrape Azure Germany IP range endpoint, error was {e}')
       return None
 
   def get_gov_range(self):
     try:
-      URL = 'https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200316.json'
+      URL = 'https://download.microsoft.com/download/6/4/D/64DB03BF-895B-4173-A8B1-BA4AD5D4DF22/ServiceTags_AzureGovernment_20200323.json'
       r = requests.get(url=URL)
       data = r.json()
       return data
     except requests.exceptions.RequestException as e:
-      print(f'Failure to scrape AWS IP range endpoint, error was {e}')
+      print(
+          f'Failure to scrape Azure Gov IP range endpoint, error was {e}')
       return None
