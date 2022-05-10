5ddd3a1a8135ee8280a346d006efd84cbb6a83c3
fartbagxp <fartbagxp@gmail.com>
2022-05-10 01:41:28 -0400

    Adding grafana needs (#62)

---
+++ src/dl/download_grafana.py
@@ -0,0 +1,80 @@
+# Documentation can be found here: https://grafana.com/docs/grafana-cloud/reference/allow-list/
+
+import requests
+
+class GrafanaCidrDownloader():
+
+  def get_hosted_alerts_range(self):
+    try:
+      URL = 'https://grafana.com/api/hosted-alerts/source-ips.txt'
+      r = requests.get(url=URL)
+      data = r.text
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Grafana IP range endpoint, error was {e}')
+      return None
+
+  def get_hosted_grafana_range(self):
+    try:
+      URL = 'https://grafana.com/api/hosted-grafana/source-ips.txt'
+      r = requests.get(url=URL)
+      data = r.text
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Grafana IP range endpoint, error was {e}')
+      return None
+
+  def get_hosted_metrics_range(self):
+    try:
+      URL = 'https://grafana.com/api/hosted-metrics/source-ips.txt'
+      r = requests.get(url=URL)
+      data = r.text
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Grafana IP range endpoint, error was {e}')
+      return None
+
+  def get_hosted_traces_range(self):
+    try:
+      URL = 'https://grafana.com/api/hosted-traces/source-ips.txt'
+      r = requests.get(url=URL)
+      data = r.text
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Grafana IP range endpoint, error was {e}')
+      return None
+
+  def get_hosted_logs_range(self):
+    try:
+      URL = 'https://grafana.com/api/hosted-logs/source-ips.txt'
+      r = requests.get(url=URL)
+      data = r.text
+      return data
+    except requests.exceptions.RequestException as e:
+      print(f'Failure to scrape Grafana IP range endpoint, error was {e}')
+      return None
+
+  def get_dns_names(self):
+    pass
+    # sm-amsterdam.grafana.net
+    # sm-atlanta.grafana.net
+    # sm-bangalore.grafana.net
+    # sm-capetown.grafana.net
+    # sm-dallas.grafana.net
+    # sm-frankfurt.grafana.net
+    # sm-london.grafana.net
+    # sm-mumbai.grafana.net
+    # sm-newark.grafana.net
+    # sm-newyork.grafana.net
+    # sm-northcalifornia.grafana.net
+    # sm-northvirginia.grafana.net
+    # sm-ohio.grafana.net
+    # sm-oregon.grafana.net
+    # sm-paris.grafana.net
+    # sm-sanfrancisco.grafana.net
+    # sm-saopaulo.grafana.net
+    # sm-seoul.grafana.net
+    # sm-singapore.grafana.net
+    # sm-sydney.grafana.net
+    # sm-tokyo.grafana.net
+    # sm-toronto.grafana.net
